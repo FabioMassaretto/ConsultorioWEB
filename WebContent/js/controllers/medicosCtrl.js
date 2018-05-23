99e33f94c92b8debadfab174a1380e1120101569
FABIO MASSARETTO <fmassaretto@gmail.com>
2018-05-23 00:27:56 -0300

    Implementing delete function

--- WebContent/js/controllers/medicosCtrl.js
+++ WebContent/js/controllers/medicosCtrl.js
@@ -1,22 +1,29 @@
-angular.module("app").controller("medicosCtrl", function ($scope, $http) {
+angular.module("consultorio").controller("medicosCtrl", function ($scope, $http) {
 	$scope.app = "Lista Telefonica";
 	$scope.medicos = [];
 	$scope.teste = "d";
 	
 	var carregarMedicos = function () {
-		$http.get("http://localhost:100/ConsultorioAPI/medicos").success(function (data) {
-			$scope.medicos = data;
-		}).error(function (data, status) {
-			$scope.message = "Aconteceu um problema: " + data;
+		$http({
+			method: 'GET',
+			url: 'http://localhost:100/ConsultorioAPI/medicos'
+		}).then(function (response) {
+			$scope.medicos = response.data;
+			console.log(response);
+		},function (error) {
+			$scope.message = "Aconteceu um problema: " + error;
 		});
 	};
 
 	$scope.adicionarMedico = function (medico) {
 		var medicoJson = JSON.stringify(medico)
-		$http.post("http://localhost:100/ConsultorioAPI/medicos", medicoJson).success(function (data) {
+		$http.post("http://localhost:100/ConsultorioAPI/medicos", medicoJson)
+		.then(function (response){
 			delete $scope.medicos;
 			$scope.medicoForm.$setPristine();
 			carregarMedicos();
+		},function (error){
+			$scope.message = "Aconteceu um problema: " + error;
 		});
 	};
 	$scope.apagarMedicos = function (medicos) {
@@ -32,6 +39,34 @@ angular.module("app").controller("medicosCtrl", function ($scope, $http) {
 	$scope.ordenarPor = function (campo) {
 		$scope.criterioDeOrdenacao = campo;
 		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+	};
+	
+	$scope.apagar = function(medicoSelecionado){
+		var medicoJson = JSON.stringify(medicoSelecionado)
+		
+		$http({
+			method: 'DELETE',
+			url: 'http://localhost:100/ConsultorioAPI/medicos',
+			data: medicoJson,
+		    headers: {
+		        'Content-type': 'application/json;charset=utf-8'
+		    }
+		}).then(function(response) {
+			delete $scope.medicos;
+			$scope.medicoForm.$setPristine();
+			carregarMedicos();
+		}, function (error){
+			$scope.message = "Aconteceu um problema: " + error;
+		});
+		
+		/*$http.delete("http://localhost:100/ConsultorioAPI/medicos", medicoJson)
+		.then(function (response){
+			delete $scope.medicos;
+			$scope.medicoForm.$setPristine();
+			carregarMedicos();
+		},function (error){
+			$scope.message = "Aconteceu um problema: " + error;
+		});*/
 	};
 
 	carregarMedicos();
