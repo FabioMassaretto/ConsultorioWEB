e8755669b8404bf9024cd13673936a2b2ce0a294
FABIO MASSARETTO <fmassaretto@gmail.com>
2018-05-23 00:26:44 -0300

    Layout small changes; adjusting page route

--- WebContent/js/config/routeConfig.js
+++ WebContent/js/config/routeConfig.js
@@ -1,16 +1,16 @@
-angular.module("app").config(function ($routeProvider) {
-	$routeProvider.when("/medicos", {
-		templateUrl: "view/medicos.html",
-		controller: "medicosCtrl",
-	});
-	$routeProvider.when("/novoContato", {
-		templateUrl: "view/novoContato.html",
-		controller: "novoContatoCtrl",
-	});
-	$routeProvider.when("/detalhesContato/:id", {
-		templateUrl: "view/detalhesContato.html",
-		controller: "detalhesContatoCtrl",
-
-	});
-	$routeProvider.otherwise({redirectTo: "/"});
+angular.module("consultorio", ["ngRoute"])
+.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "view/partial/inicio.html"
+    })
+    .when("/medicos", {
+        templateUrl : "view/medicos.html",
+        controller : "medicosCtrl"
+    })
+    .when("/paris", {
+        templateUrl : "paris.htm",
+        controller : "parisCtrl"
+    });
+    $routeProvider.otherwise({redirectTo: "/"});
 });
